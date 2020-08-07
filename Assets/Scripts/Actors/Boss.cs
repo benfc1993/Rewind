47a883b0bef4fc5cea63db2a2e944168617db5d3
benfc <benfeldbergcollins@hotmail.co.uk>
2020-08-07 17:55:02 +0100

    Boss level

---
+++ Assets/Scripts/Actors/Boss.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+	public ParticleSystem DeathEffect;
+	public GameObject[] computers;
+
+	private void Start()
+	{
+		computers = GameObject.FindGameObjectsWithTag("Computer");
+	}
+	private void OnTriggerEnter(Collider other)
+	{
+		print("collision");
+		if(other.tag == "Bullet")
+		{
+			foreach(GameObject c in computers)
+			{
+				print(c);
+				c.GetComponent<Computer>().Explode(DeathEffect);
+			}
+			Time.timeScale = 0.5f;
+			GetComponent<AudioSource>().Play();
+			Destroy(Instantiate(DeathEffect.gameObject, transform.position, transform.rotation) as GameObject, DeathEffect.main.startLifetimeMultiplier);
+			GetComponent<MeshRenderer>().enabled = false;
+		}
+	}
+}
---
+++ Assets/Scripts/Actors/Computer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Computer : MonoBehaviour
+{
+	bool hasExploded = false;
+	public void Explode(ParticleSystem DeathEffect)
+	{
+		if(!hasExploded)
+		{
+			hasExploded = true;
+			StartCoroutine(Death(DeathEffect));
+		}
+	}
+	IEnumerator Death(ParticleSystem DeathEffect)
+	{
+		float delay = Random.Range(0,1.5f);
+		yield return new WaitForSeconds(0);
+		GetComponent<AudioSource>().Play();
+		Destroy(Instantiate(DeathEffect.gameObject, transform.position, transform.rotation) as GameObject, DeathEffect.main.startLifetimeMultiplier);
+		GetComponent<MeshRenderer>().enabled = false;
+	}
+}
